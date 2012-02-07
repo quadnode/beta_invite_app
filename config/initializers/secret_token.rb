181274d312df79f306a761975db6cfb3bd6f6fa8
harsha <harsha@node1-Aspire-5742.(none)>
2012-02-07 11:03:04 +0530

    rails 3 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BetaInvite::Application.config.secret_token = 'f6da287d5b308d7a5c2dc95a019ac23f849242b0b6105303f1df9fab7cbdb2b0e4c024786c4853be31a8fc836c90cc43327e63e9db2ae8340d0401fdd74c6520'
